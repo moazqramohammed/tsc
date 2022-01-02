a6cc4fcd8609c090d4beefbc18c58afc9b71d961
omarqra <ammarqm5@gmail.com>
2022-01-02 12:05:05 +0200

    truck order 1

--- pages/order_truck/index.jsx
+++ pages/order_truck/index.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import MainHeader from "../../components/mainHeader";
 import MainFooter from "../../components/mainFooter";
 import styles from "../../styles/order_truck.module.scss";
 import Image from "next/image";
+import img from "../../public/images/شاحنة بيضاء عميق.jpg";
 
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
+import ar from "react-phone-input-2/lang/ar.json";
+
+const formData = {
+  name: "",
+  type: "",
+  destination: "",
+  origin: "",
+  number: "",
+  email: "",
+};
 const index = () => {
+  const [update, setUpdate] = useState(0);
+  const [number, setNumber] = useState("");
   return (
     <div>
       <MainHeader NotMain={true} page={"truck_order"} />
@@ -15,8 +30,9 @@ const index = () => {
         <div className={styles.container}>
           <div>
             <form
-              onSubmit={() => {
-                console.log("تم تسليم الفورم");
+              onSubmit={(e) => {
+                e.preventDefault();
+                console.log(formData);
               }}
             >
               <ul>
@@ -24,46 +40,104 @@ const index = () => {
                   <span>
                     <abbr title='هذا الحقل مطلوب'>*</abbr>
                   </span>
-                  <input type='text' placeholder='أدخل اسمك هنا' />
+                  <input
+                    type='text'
+                    placeholder='أدخل اسمك هنا'
+                    onChange={(e) => {
+                      formData.name = e.target.value;
+                      setUpdate(update + 1);
+                    }}
+                    value={formData.name}
+                  />
                 </li>
                 <li>
                   <span>
                     <abbr title='هذا الحقل مطلوب'>*</abbr>
                   </span>
-                  <input type='text' placeholder='أدخل نوع البضاعة' />
+                  <input
+                    type='text'
+                    placeholder='أدخل نوع البضاعة'
+                    onChange={(e) => {
+                      formData.type = e.target.value;
+                      setUpdate(update + 1);
+                    }}
+                    value={formData.type}
+                  />
                 </li>
                 <li>
                   <span>
                     <abbr title='هذا الحقل مطلوب'>*</abbr>
                   </span>
-                  <input type='text' placeholder='أدخل الوجهة' />
+                  <input
+                    type='text'
+                    placeholder='أدخل الوجهة'
+                    onChange={(e) => {
+                      formData.destination = e.target.value;
+                      setUpdate(update + 1);
+                    }}
+                    value={formData.destination}
+                  />
                 </li>
                 <li>
                   <span>
                     <abbr title='هذا الحقل مطلوب'>*</abbr>
                   </span>
-                  <input type='text' placeholder='أدخل مكان الانطلاق' />
+                  <input
+                    type='text'
+                    placeholder='أدخل مكان الانطلاق'
+                    onChange={(e) => {
+                      formData.origin = e.target.value;
+                      setUpdate(update + 1);
+                    }}
+                    value={formData.origin}
+                  />
+                </li>
+                <li>
+                  <span className={styles.numberSpan}>
+                    <abbr title='هذا الحقل مطلوب'>*</abbr>
+                  </span>
+                  <PhoneInput
+                    containerClass={styles.numberInput}
+                    buttonClass={styles.dropButton}
+                    localization={ar}
+                    value={formData.number}
+                    onChange={(e) => {
+                      formData.number = e;
+                      setUpdate(update + 1);
+                    }}
+                  />
+                  {/* <input
+                    type='tel'
+                    placeholder='أدخل رقم الهاتف'
+                    onChange={(e) => {
+                      formData.number = e.target.value;
+                      setUpdate(update + 1);
+                    }}
+                    value={formData.number}
+                  /> */}
                 </li>
                 <li>
                   <span>
                     <abbr title='هذا الحقل مطلوب'>*</abbr>
                   </span>
-                  <input type='text' placeholder='أدخل رقم الهاتف' />
-                </li>
-                <li>
-                  <span>
-                    <abbr title='هذا الحقل مطلوب'>*</abbr>
-                  </span>
-                  <input type='text' placeholder='أدخل الإيميل' />
+                  <input
+                    type='text'
+                    placeholder='أدخل الإيميل'
+                    onChange={(e) => {
+                      formData.email = e.target.value;
+                      setUpdate(update + 1);
+                    }}
+                    value={formData.email}
+                  />
                 </li>
               </ul>
-              <input type='button' value='تقديم الطلب' />
+              <input type='submit' value='تقديم الطلب' />
             </form>
           </div>
 
           <div>
             <Image
-              src='/images/شاحنة بيضاء عميق.jpg'
+              src={img}
               alt='شاحنة بيضاء عميق'
               layout='fill'
               objectFit='cover'
